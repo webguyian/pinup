50296594525af1d7bbb9ae660926861b63663bf5
Ian Mac <webguyian@gmail.com>
2013-04-01 19:07:00 -0400

    Connect Amazon S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,4 +67,14 @@ Pinup::Application.configure do
 
   # Change in production to host
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  # Configure Amazon S3 for File Upload with Paperclip
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 end
